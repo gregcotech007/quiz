07bedaf16ce736490b455b914e0ea52ee376a73c
gregcotech007 <commins.greg@outlook.com>
2022-03-06 21:07:16 +0000

    Request input answer from user

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-# Create a Python Quiz with a choice of 3 multiple choice answers
-
 questions = {
     "Who sang the title song for the latest Bond film, No Time to Die?: ": "c",
     "What Italian city is famous for its system of canals?: ": "c"
@@ -10,16 +8,22 @@ answer_choice = [
     ["a. Rome", "b. Naples", "c. Venice"],
     ]
 
-
 def new_game():
+    """
+    Call questions and mutiple choice answers to display to user
+    """
+
     answers = []
     correct_answers = 0
     question_num = 1
 
     for key in questions:
         print(key)
-        for i in answer_choice[question_num -1]:
+        for i in answer_choice[question_num-1]:
             print(i)
-
+        
+        answer = input("Enter a, b, or c: ")
+        answer = answer.lower()
+        answers.append(answer)
 
 new_game()
