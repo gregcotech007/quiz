753ff0d6add5ea78cbc157a7bce41a899d328371
gregcotech007 <commins.greg@outlook.com>
2022-03-06 20:43:14 +0000

    Call questions and answer choice to user

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 # Create a Python Quiz with a choice of 3 multiple choice answers
+
 questions = {
     "Who sang the title song for the latest Bond film, No Time to Die?: ": "c",
     "What Italian city is famous for its system of canals?: ": "c"
@@ -8,3 +9,17 @@ answer_choice = [
     ["a. Adele", "c. Sam Smith", "c. Billie Eilish"],
     ["a. Rome", "b. Naples", "c. Venice"],
     ]
+
+
+def new_game():
+    answers = []
+    correct_answers = 0
+    question_num = 1
+
+    for key in questions:
+        print(key)
+        for i in answer_choice[question_num -1]:
+            print(i)
+
+
+new_game()
