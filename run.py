3554d6ae12b5ce6206b179737f04f52cfd7470d5
gregcotech007 <commins.greg@outlook.com>
2022-03-06 20:15:22 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,2 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Create a Python Quiz with a choice of 3 multiple choice answers
+
