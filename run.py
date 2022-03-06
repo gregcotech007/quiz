d5f6e5f085618fd530186fa7b37610b60a1d0021
gregcotech007 <commins.greg@outlook.com>
2022-03-06 20:18:44 +0000

    Added 2 quiz questions and 2 answer options

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 # Create a Python Quiz with a choice of 3 multiple choice answers
+questions = {
+    "Who sang the title song for the latest Bond film, No Time to Die?: ": "c",
+    "What Italian city is famous for its system of canals?: ": "c"
+}
 
+answer_choice = [
+    ["a. Adele", "c. Sam Smith", "c. Billie Eilish"],
+    ["a. Rome", "b. Naples", "c. Venice"],
+    ]
